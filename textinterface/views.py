0fd00401fc06762795524fa564a3a5c5971937a5
anapmc.carvalho@gmail.com <anapmc.carvalho@gmail.com>
2015-06-22 12:43:53 +0100

    text interface static

--- textinterface/views.py
+++ textinterface/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def index(request):
-	return render(request, 'textinterface/index.html', {})
+	return render(request, 'textinterface/index.html', {}) #modified
     
