fb2bf4bf1bed1cd920a38df4fbfe487da446243b
anapmc.carvalho@gmail.com <anapmc.carvalho@gmail.com>
2015-06-24 14:18:04 +0100

    ajax request conds

--- vcgen/views.py
+++ vcgen/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
-def getconds(request):
-	return HttpResponse("hello")
+def getconds(request,jsontxt):
+	return render(request, 'textinterface/index.html', {})
