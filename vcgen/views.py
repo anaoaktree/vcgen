1ebc04d2f7398c2d0b47cc7f1b433afeab833fe5
anapmc.carvalho@gmail.com <anapmc.carvalho@gmail.com>
2015-06-24 14:45:56 +0100

    ajax request conds

--- vcgen/views.py
+++ vcgen/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def getconds(request):
-	print "request mything", request.body, request.request
+	print "request mything", request.body, request["code"]
 	return HttpResponse("heybi")
