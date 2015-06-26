6075d02c4c66929fc5b110b15f989a388cc455fd
anapmc.carvalho@gmail.com <anapmc.carvalho@gmail.com>
2015-06-26 09:44:05 +0100

    django cors headers setts

--- vcgen/views.py
+++ vcgen/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse, JsonResponse
 # Create your views here.
 def getconds(request):
 	print "request mything", request.body
-	return HttpResponse("heybi" + request.read()+ "+  body:"+ request.body)
+	var objs= request.body
+	return HttpResponse("heybi" + type(objs))
