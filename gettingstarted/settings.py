4bf351a292c2cfd50dd2ea62cedf10a2fa6644b5
anapmc.carvalho@gmail.com <anapmc.carvalho@gmail.com>
2015-06-26 09:06:53 +0100

    django cors headers setts

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -39,12 +39,14 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'vcgen',
     'textinterface'
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
