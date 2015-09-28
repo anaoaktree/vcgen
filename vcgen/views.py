8df0f44d9c97bc43a0786cddad0e68c2b653901a
anapmc.carvalho@gmail.com <anapmc.carvalho@gmail.com>
2015-09-28 14:06:24 +0100

    csrf cookie problem

--- vcgen/views.py
+++ vcgen/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import ensure_csrf_cookie
 import json, elsa
 # Create your views here.
+
+@ensure_csrf_cookie
 def getconds(request):
 	result = elsa.start(request.body)
 	return HttpResponse(result)
